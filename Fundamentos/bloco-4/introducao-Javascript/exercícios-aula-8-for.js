50e2a50edfc90001a104a756ddc429ff0a2c0901
Edvaldo Alves do Nascimento <edvaldodentt@gmail.com>
2022-05-19 10:37:54 -0300

    Estudos3

---
+++ Fundamentos/bloco-4/introducao-Javascript/exercícios-aula-8-for.js
@@ -0,0 +1,18 @@
+
+
+// Exercício - 1
+// Nesse primeiro exercício, percorra o array imprimindo todos os valores nele contidos com a função console.log();
+
+// console.log(numbers);
+
+// Para o segundo exercício, some todos os valores contidos no array e imprima o resultado;
+
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// let resultado = 0
+
+// for (let i = 0 ; i < numbers.length; i += 1){
+//     resultado = resultado + numbers[i];
+// }
+// console.log(resultado);
+
+
