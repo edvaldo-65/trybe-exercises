5b2a8bc8ba1a1e2ad13656ed837b40b9d79aa035
Edvaldo Alves do Nascimento <edvaldodentt@gmail.com>
2022-07-24 05:38:40 -0300

    pokedex

--- Front-End/Bloco 10/my-pokedex/src/components/fancy-buttons.jsx
+++ Front-End/Bloco 10/my-pokedex/src/components/fancy-buttons.jsx
@@ -1,8 +1,5 @@
 import React from 'react';
 
-function handleClick() {
-   console.log('Clicou no botão');
-}
 
 function handleClick2() {
     console.log('Clicou no botão 2');
@@ -10,14 +7,27 @@ function handleClick2() {
 
 function handleClick3() {
     console.log('Clicou no botão 3');
-
+    
 }
 
 class FancyButton extends React.Component {
+constructor() {
+    super()
+    this.handleClick1 = this.handleClick1.bind(this);
+    this.state = {
+        numeroDeClicks: 0
+    }
+    // console.log('Eu aqui desmistificando o constructor')
+}
+ handleClick1() {
+    console.log(this);
+       console.log('Clicou no botão 1');
+    }
     render() {
+        console.log(this);
         return (
             <>
-            <button onClick={handleClick}>Meu botão</button>
+            <button onClick={this.handleClick1}>Meu botão1</button>
             <button onClick={handleClick2}>Meu botão2</button> 
             <button onClick={handleClick3}>Meu botão3</button>              
             </>
